ad8fd160d97d200c6187e21f12eea1d9c83aaa33
LuciZR <151607296+LuciZR@users.noreply.github.com>
2024-03-02 23:22:18 +0530

    𝙼𝙸𝙰 𝙼𝙳 ⚠︎

---
+++ plugins/main-Guru.js
@@ -0,0 +1,32 @@
+let handler = async m => m.reply(`
+
+≡  *𝑴𝑰𝑨  ┃ ᴍᴅ*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+https://chat.whatsapp.com/HuQXSSgC80hERPfi4D4D2i
+
+▢ Group 2
+https://chat.whatsapp.com/L9kqKKZpQY2BrqHabqfa0I
+
+▢ Group 3
+https://chat.whatsapp.com/IyEWsH5yjEQ58PTIAkWifp
+
+─────────────
+
+▢ Group WhatsApp 
+ https://chat.whatsapp.com/HuQXSSgC80hERPfi4D4D2i
+─────────────
+▢ *Owner Telegram*
+ https://t.me/LUCIZR
+
+▢ *Instagram*
+• https://www.instagram.com/i.luci.zr
+
+
+`.trim())
+handler.help = ['gpmia']
+handler.tags = ['main']
+handler.command = ['groups', 'groupmia', 'miagp', 'lucigp', 'gpmia'] 
+
+export default handler
